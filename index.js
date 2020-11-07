db3fe69711ad743e56df0b4bb98a736e1ba97ec6
danagm <43969499+danagm@users.noreply.github.com>
2020-11-07 08:54:01 -0600

    my chat working well

---
+++ index.js
@@ -0,0 +1,24 @@
+var s = process.env.PORT||4000;
+var express=require('express');
+var socket =require('socket.io');
+//
+var app =express();
+
+var server = app.listen(s, function(){
+});
+
+
+app.use(express.static('public'));
+//files
+var io=socket(server);
+
+io.on('connection',function(socket){
+    console.log('hehco la conexion',socket.id)
+    socket.on('chat',function(data){
+        io.sockets.emit('chat',data);
+    });
+    
+    socket.on('typing',function(data){
+        socket.broadcast.emit('typing',data)
+    });
+});
